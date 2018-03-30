3b7f2a5fe7d3be9576d7eea071f504bcbce113e6
mclark1990 <mjc8875@gmail.com>
2018-03-30 19:19:49 -0400

    file before css styling

---
+++ assets/javascript/game.js
@@ -0,0 +1,68 @@
+// guess options for computer
+var computerChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+// variables for user
+
+var wins = 0
+    losses = 0
+    guessesNum = 9
+    totalGuesses = [];
+
+var html = 
+        "<p>Wins: " + wins + "</p>" +
+        "<p>Losses: " + losses + "</p>" +
+        "<p>Guesses Left: " + guessesNum + "</p>"+
+        "<p> Your Guesses So Far: " + totalGuesses + "</p>";
+var ScoreText = getElementById("game").innerHTML = html;               
+       
+document.onload = function(event){
+    
+}                
+        
+
+//function runs when user presses a key
+
+document.onkeyup = function(event){
+
+// determines what key user pushed
+var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+console.log(userGuess);
+// Randomly chooses a letter from computer choices
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+console.log(computerGuess);
+
+if (userGuess === computerGuess){
+    wins++;
+    guessesNum = 9;
+    totalGuesses = [];
+ 
+    console.log(guessesNum);
+}
+
+if (userGuess != computerGuess){
+   guessesNum--;
+   totalGuesses.push(" " + userGuess); 
+}
+
+if (guessesNum === 0){
+    losses++;
+    totalGuesses = [];
+    guessesNum = 9;
+}
+
+
+var html = 
+        "<p>Wins: " + wins + "</p>" +
+        "<p>Losses: " + losses + "</p>" +
+        "<p>Guesses Left: " + guessesNum + "</p>"+
+        "<p> Your Guesses So Far: " + totalGuesses + "</p>";
+
+document.querySelector("#game").innerHTML = html;
+
+};
+        
+
+
+
